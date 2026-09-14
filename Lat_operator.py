24b2e9c82ffc7bf7560429b8b45b654ffad39914
Rosaliadwirahmawanti <rrosarplkanesa@gmail.com>
2026-09-14 10:08:58 +0700

    Mendaftarkan file HTML dan CSS profil digital

---
+++ Lat_operator.py
@@ -0,0 +1,16 @@
+a = 10
+b = 4
+
+penjumlahan = a + b
+pengurangan = a - b
+perkalian = a * b
+pembagian = a / b
+modulus = a % b
+pemangkatan = a ** b 
+
+print("hasil penjumlahan",penjumlahan)
+print("hasil pengurangan",pengurangan)
+print("hasil perkalian",perkalian)
+print("hasil pembagian",pembagian)
+print("hasil modulus",modulus)
+print("hasil pemangkatan",pemangkatan)
---
+++ Latihan.1.py
@@ -0,0 +1,9 @@
+nama = input("masukkan nama anda : ")
+tinggi_badan = input("masukkan tinggi badan anda : ")
+umur = input("masukan umur anda : ")
+golongan_darah = input("masukan golonggan darah anda : ")
+
+print("Nama saya adalah :",nama)
+print("Saya memiliki tinggi badan : ",tinggi_badan, "cm")
+print("Umur saya adalah :",umur," tahun")
+print("Golongan darah saya adalah : ",golongan_darah)
